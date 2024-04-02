c2e694fa1587bb5415ac491c4aecde18310e0111
FatemaGirnary <fatema_125@hotmail.com>
2024-04-02 13:31:38 +0800

    created folders

---
+++ PyBank/main.py
@@ -0,0 +1,12 @@
+#Your task is to create a Python script that analyses the records to calculate each of the following values:
+#The total number of months included in the dataset
+#The net total amount of "Profit/Losses" over the entire period
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+#The greatest increase in profits (date and amount) over the entire period
+#The greatest decrease in profits (date and amount) over the entire period
+
+import os   
+import csv
+
+csvpath =os.path.join("Resources", "budget_data.csv")
+
