eadf96209020f54007637c0f99c7e14e8dfa60a6
FatemaGirnary <fatema_125@hotmail.com>
2024-04-05 11:56:40 +0800

    PyBank code added

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,8 +5,62 @@
 #The greatest increase in profits (date and amount) over the entire period
 #The greatest decrease in profits (date and amount) over the entire period
 
-import os   
+import os
 import csv
 
-csvpath =os.path.join("Resources", "budget_data.csv")
+#file path
+csvpath = os.path.join('Resources','budget_data.csv')
 
+total_months = 0
+net_total = 0
+prev_profit_loss = 0
+total_changes = 0
+greatest_increase = {"date": "", "amount": float("-inf")}
+greatest_decrease = {"date": "", "amount": float("inf")}
+changes = []
+
+# Read the CSV file
+with open(csvpath, 'r') as file:
+    csv_reader = csv.DictReader(file)
+
+#Iterate through each row
+    for row in csv_reader:
+
+#Extract date and profit/losses
+        date = row['Date']
+        profit_loss = int(row['Profit/Losses'])
+
+# Calculate total number of months from each row of data after the header
+        total_months += 1
+
+#Calculate the net total amount of "Profit/Losses" over the entire period
+        net_total += profit_loss
+
+#Calculate the changes in "Profit/Losses" over the entire period, and then the average of those changes
+        if total_months > 1:
+            change = profit_loss - prev_profit_loss
+            total_changes += change
+            changes.append(change)
+
+#The greatest increase in profits (date and amount) over the entire period
+            if change > greatest_increase["amount"]:
+                greatest_increase["date"] = date
+                greatest_increase["amount"] = change
+
+#The greatest decrease in profits (date and amount) over the entire period
+            if change < greatest_decrease["amount"]:
+                greatest_decrease["date"] = date
+                greatest_decrease["amount"] = change
+
+        prev_profit_loss = profit_loss
+
+# Calculate the average of those changes
+average_change = total_changes / (total_months-1)
+
+print ("Financial Analysis")
+print ("--------------------")
+print (f"Total Months: {total_months}")
+print (f"Total: ${net_total}")
+print (f"Average Change: ${average_change: .2f}")
+print (f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
+print (f"Greatest Decrease in Profits: {greatest_decrease['date']} (${greatest_decrease['amount']})")
